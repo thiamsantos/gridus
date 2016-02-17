206b3bee593c15366926599408cfbd2376bdb351
thiamdossantos <thia.mdossantos@gmail.com>
2016-02-17 12:00:24 -0200

    mixin created

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,8 @@ var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 var include = require('gulp-include');
 var gutil = require('gulp-util');
-var jshint = require('gulp-jshint');
-var sourcemaps = require('gulp-sourcemaps');
 var csscss = require('gulp-csscss');
 var browserSync = require('browser-sync').create();
-var del = require('del');
 
 var mainjs = './src/js/main.js';
 var js = './src/js/**';
@@ -30,28 +27,6 @@ gulp.task('browser-sync', function() {
   watch("./dist/**").on('change', browserSync.reload);
 });
 
-// Javascript task
-gulp.task('dev-js-task', function() {
-  gulp.src(mainjs)
-    .pipe(watch(js))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(sourcemaps.init())
-    .pipe(include())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe(browserSync.stream());
-});
-gulp.task('prod-js-task', function() {
-  gulp.src(mainjs)
-    .pipe(watch(js))
-    .pipe(include())
-    .pipe(uglify().on('error', gutil.log))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe(browserSync.stream());
-  del('./dist/js/**/*.map');
-});
-
 // Sass Task
 gulp.task('dev-sass-task', function() {
   gulp.src(css)
@@ -68,30 +43,6 @@ gulp.task('prod-sass-task', function() {
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css/'))
     .pipe(browserSync.stream());
-  del('./dist/css/**/*.map');
-});
-
-// Csscss Task
-gulp.task('csscss-task', function() {
-  gulp.src(css)
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(csscss())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(browserSync.stream());
-});
-
-
-// Images tasks
-gulp.task('img-task', function() {
-  gulp.src(img)
-    .pipe(watch(img))
-    .pipe(imagemin({
-      progressive: true,
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest('./dist/img'));
 });
 
 // Task runners
