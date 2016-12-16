9bf7f2ea72156c92bbd55c27e9be8152ac3e2688
thiamsantos <thia.mdossantos@gmail.com>
2016-12-16 16:31:48 -0200

    update index and reorganize

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var path = require('path')
 var pkg = require('./package.json')
 
+var gridusPath = path.join(__dirname, '/gridus.styl')
+
 exports = module.exports = function (opts) {
+  var implicit = (opts && opts.implicit == false) ? false : true;
+
   return function (style) {
-    style.include(__dirname)
+    style.include(__dirname);
+
+    if (implicit) {
+      style.import(gridusPath);
+    }
   }
 }
 
 exports.libname = pkg.name
+exports.path = gridusPath
 exports.version = pkg.version
